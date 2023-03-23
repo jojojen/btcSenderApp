9fd1d6d2c1cb1ceb062b1022df53de873679564e
jen <jenhung.jocker@gmail.com>
2023-03-23 14:56:03 +0800

    add BTC-USD converter

--- btc-app-angular/src/app/app.component.ts
+++ btc-app-angular/src/app/app.component.ts
@@ -9,6 +9,7 @@ import axios from 'axios';
 export class AppComponent {
   btcAddress!: string;
   balance: number | null = null;
+  usdValue: number | null = null;
   error: string | null = null;
 
   async checkBalance() {
@@ -20,8 +21,19 @@ export class AppComponent {
       const response = await axios.get(`https://blockchain.info/rawaddr/${this.btcAddress}`);
       this.balance = response.data.final_balance / 100000000;
       this.error = null;
+      await this.calculateUsdValue();
     } catch (error) {
       this.error = 'Error fetching balance. Please try again later.';
+    }
+  }
+
+  async calculateUsdValue() {
+    try {
+      const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json');
+      const btcToUsdRate = response.data.bpi.USD.rate_float;
+      this.usdValue = this.balance! * btcToUsdRate;
+    } catch (error) {
+      this.error = 'Error fetching BTC to USD conversion rate. Please try again later.';
     }
   }
 }
