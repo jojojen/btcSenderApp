9a9dd8c55a2c1d93beabd5443b6ad0c4886320ae
jen <jenhung.jocker@gmail.com>
2023-03-27 14:23:04 +0800

    add transaction fee estimator

--- btc-app-angular/src/app/app.component.ts
+++ btc-app-angular/src/app/app.component.ts
@@ -12,20 +12,37 @@ export class AppComponent {
   usdValue: number | null = null;
   error: string | null = null;
 
-  async checkBalance() {
-    if (!this.btcAddress) {
-      this.error = 'Please enter a valid Bitcoin address.';
-      return;
-    }
-    try {
-      const response = await axios.get(`https://blockchain.info/rawaddr/${this.btcAddress}`);
-      this.balance = response.data.final_balance / 100000000;
-      this.error = null;
-      await this.calculateUsdValue();
-    } catch (error) {
-      this.error = 'Error fetching balance. Please try again later.';
-    }
+  transactionFeeBtc: number | null = null;
+  transactionFeeUsd: number | null = null;
+
+// Add a new method to fetch balance only
+async fetchBalance() {
+  if (!this.btcAddress) {
+    this.error = 'Please enter a valid Bitcoin address.';
+    return;
   }
+  try {
+    const response = await axios.get(`https://blockchain.info/rawaddr/${this.btcAddress}`);
+    this.balance = response.data.final_balance / 100000000;
+    this.error = null;
+    await this.calculateUsdValue();
+  } catch (error) {
+    this.error = 'Error fetching balance. Please try again later.';
+  }
+}
+
+// Add a new method to fetch transaction fees only
+async fetchTransactionFees() {
+  try {
+    const feePerByte = await this.fetchTransactionFeeEstimate();
+    const averageTransactionSize = 250; // You can adjust this value based on the type of transaction
+    this.transactionFeeBtc = feePerByte * averageTransactionSize / 100000000;
+    this.transactionFeeUsd = this.transactionFeeBtc * (this.usdValue! / this.balance!);
+  } catch (error) {
+    this.error = 'Error fetching transaction fees. Please try again later.';
+  }
+}
+
 
   async calculateUsdValue() {
     try {
@@ -36,4 +53,15 @@ export class AppComponent {
       this.error = 'Error fetching BTC to USD conversion rate. Please try again later.';
     }
   }
+
+  async fetchTransactionFeeEstimate() {
+    try {
+      const response = await axios.get('https://mempool.space/api/v1/fees/recommended');
+      const fastestFeePerByte = response.data.fastestFee;
+      return fastestFeePerByte;
+    } catch (error) {
+      this.error = 'Error fetching transaction fee estimate. Please try again later.';
+    }
+  }
+  
 }
